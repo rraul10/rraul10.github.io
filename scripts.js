de4ab746ebeed47a875f0bc17419cc909d5ffdc9
Raúl Fernández <146001066+rraul10@users.noreply.github.com>
2024-11-05 18:51:14 +0100

    Subida pantalla de carga

--- scripts.js
+++ scripts.js
@@ -4,10 +4,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Esperar hasta que la página esté completamente cargada
     window.addEventListener("load", () => {
-        // Ocultar el loader
-        loader.style.display = "none";
-        
-        // Mostrar el contenido principal
-        content.style.opacity = "1";
+        // Mantener el loader visible durante 5 segundos
+        setTimeout(() => {
+            // Ocultar el loader
+            loader.style.display = "none";
+            
+            // Mostrar el contenido principal
+            content.style.opacity = "1";
+        }, 5000); // 5000 milisegundos = 5 segundos
     });
 });
